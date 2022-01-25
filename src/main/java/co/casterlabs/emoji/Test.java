8dcc3507af112eef9a7df65a7e888b82cc34ef5c
Ender <33337309+e3ndr@users.noreply.github.com>
2022-01-24 19:05:50 -0600

    Get the Sora testbed working.

--- src/main/java/co/casterlabs/emoji/Test.java
+++ src/main/java/co/casterlabs/emoji/Test.java
@@ -1,44 +1,19 @@
 package co.casterlabs.emoji;
 
-import co.casterlabs.emoji.data.EmojiAssets;
-import co.casterlabs.emoji.data.EmojiIndex;
-import co.casterlabs.emoji.generator.EmojiIndexGenerator;
+import co.casterlabs.emoji.api.EmojiApi;
+import co.casterlabs.sora.SoraFramework;
+import co.casterlabs.sora.SoraLauncher;
 
 public class Test {
 
     public static void main(String[] args) throws Exception {
-        EmojiIndex index = EmojiIndexGenerator.load();
+        SoraFramework framework = new SoraLauncher().buildWithoutPluginLoader();
 
-        // https://www.w3schools.com/html/tryit.asp?filename=tryhtml_basic
+        framework
+            .getSora()
+            .register(new EmojiApi());
 
-        System.out.println(
-            "<style>\n"
-                + "    body {\n"
-                + "        font-size: 2em;\n"
-                + "    }\n"
-                + "</style>\n"
-                + ""
-        );
-
-        String[] formats = {
-                "Live long and prosper. 🖖 (%s)",
-                "I'm a little teapot, short and stout 🫖 (%s)",
-                "We come in all colors of the rainbow 👋👋🏻👋🏼👋🏽👋🏾👋🏿 (%s)"
-        };
-
-        for (String format : formats) {
-            for (EmojiAssets.AssetImageProvider provider : EmojiAssets.emojiProviders) {
-                String text = String.format(format, provider.getProviderName());
-
-                System.out.println(index.matchAllEmojisAndReturnHtml(text, provider.getProviderId()));
-                System.out.println("<br />");
-            }
-
-            System.out.printf(format, "Your Browser");
-            System.out.println();
-            System.out.println("<br />");
-            System.out.println("<br />");
-        }
+        framework.startHttpServer();
     }
 
 }
